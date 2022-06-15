5e268204735ad96221941717d1811c1ceda8ec76
Laurie Baker <laurie.baker@glasgow.ac.uk>
2022-06-15 10:52:40 +0200

    Update additional slide formatting

---
+++ tutorial/package_install.R
@@ -0,0 +1,16 @@
+# install.packages("tidyverse")
+# install.packages("leaflet")
+# install.packages("sf")
+# install.packages("RColorBrewer")
+# install.packages("htmltools")
+# install.packages("leafsync")
+# install.packages("kableExtra")
+
+
+library(tidyverse) ## For plotting and data wrangling library
+library(leaflet) ## For leaflet interactive maps
+library(sf) ## For spatial data
+library(RColorBrewer) ## For colour palettes
+library(htmltools) ## For html
+library(leafsync) ## For placing plots side by side
+library(kableExtra) ## Table  output (in slides)
